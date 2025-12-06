6c0a24351f32ee5c3d8d5b81d5a3671ee7ac5636
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-12-06 22:57:23 +0200

    Tooltip for the fantasy chest

--- src/js/chest.js
+++ src/js/chest.js
@@ -1,9 +1,21 @@
 const openChestButton = document.querySelector('button.chest-open-button');
 const chestImage = document.querySelector('.chest-image-container > img');
+const chestText = document.querySelector('section.chest .chest-tooltip');
+const chestImageContainer = document.querySelector('.chest-image-container');
 
 openChestButton.addEventListener('click', openChest);
+chestImage.addEventListener('mouseenter', showChestText);
+chestImageContainer.addEventListener('mouseleave', hideChestText);
 
 function openChest() {
   chestImage.src = 'assets/images/chest-opened.png';
   chestImage.alt = 'opened-chest';
+}
+
+function showChestText() {
+  chestText.classList.remove('hidden-text');
+}
+
+function hideChestText() {
+  chestText.classList.add('hidden-text');
 }
