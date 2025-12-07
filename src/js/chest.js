6847fdfde58137fec3d44f1bbdf29457e4897df3
Anton Mirazchiyski <anton.mirazchiyski@gmail.com>
2025-12-07 15:25:44 +0200

    Refactor 'chest' file and create Chest class

--- src/js/chest.js
+++ src/js/chest.js
@@ -3,14 +3,27 @@ const chestImage = document.querySelector('.chest-image-container > img');
 const chestText = document.querySelector('section.chest .chest-tooltip');
 const chestImageContainer = document.querySelector('.chest-image-container');
 
-openChestButton.addEventListener('click', openChest);
+class Chest {
+  chestStates = {
+    closed: 'assets/images/chest-closed.png',
+    opened: 'assets/images/chest-opened.png'
+  };
+
+  open() {
+    chestImage.src = this.chestStates['opened'];
+    chestImage.alt = 'opened-chest';
+  }
+}
+
+const chest = new Chest();
+
+openChestButton.addEventListener('click', () => {
+  chest.open()
+});
+
 chestImage.addEventListener('mouseenter', showChestText);
 chestImageContainer.addEventListener('mouseleave', hideChestText);
 
-function openChest() {
-  chestImage.src = 'assets/images/chest-opened.png';
-  chestImage.alt = 'opened-chest';
-}
 
 function showChestText() {
   chestText.classList.remove('hidden-text');
